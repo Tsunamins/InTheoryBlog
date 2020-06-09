17615b2840004763ddabb16ddbce1a26554fa005
Tsunamins <reillyamr@gmail.com>
2020-06-09 09:49:33 -0400

    add server db connection routes, passport, port

---
+++ in-theory-backend/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const users = require("./routes/api/v1/users");
+const posts = require("./routes/api/v1/posts");
+
+const app = express();
+app.use(express.json());
+
+const uri = require("./config/keys").mongoURI;
+mongoose.connect(uri,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
+
+  // Passport middleware
+  app.use(passport.initialize());
+  // Passport config
+  require("./config/passport")(passport);
+  // Routes
+  app.use("/api/v1/users", users);
+  app.use("/api/v1/posts", posts)
+
+
+
+const port = process.env.PORT || 5000; //process.env.PORT is something to do w heroku launch
+app.listen(port, () => console.log(`Server running on ${port} !`));
