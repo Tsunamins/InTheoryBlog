f6c6ff78881c641048ec6ad1eeb4cf27f9a97662
Tsunamins <reillyamr@gmail.com>
2020-06-09 15:12:13 -0400

    add authReducer to store

--- in-theory-frontend/src/store.js
+++ in-theory-frontend/src/store.js
@@ -1,8 +1,11 @@
 import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import thunk from "redux-thunk";
+import authReducer from './reducers/authReducer'
 
+const reducer = combineReducers({
 
-const reducer = combineReducers({ })
+    authReducer
+ })
 
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
