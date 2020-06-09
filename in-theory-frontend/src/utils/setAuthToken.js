245f207e116f97be3283fa0f04997a3816ccc8ce
Tsunamins <reillyamr@gmail.com>
2020-06-09 12:53:05 -0400

    change to possible option, updating fetch header

--- in-theory-frontend/src/utils/setAuthToken.js
+++ in-theory-frontend/src/utils/setAuthToken.js
@@ -1,11 +1,36 @@
-import axios from "axios";
+
 const setAuthToken = token => {
+const update = {}
+//const update = { ...options };
   if (token) {
     // Apply authorization token to every request if logged in
-    axios.defaults.headers.common["Authorization"] = token;
-  } else {
-    // Delete auth header
-    delete axios.defaults.headers.common["Authorization"];
-  }
-};
+ 
+              update.headers = {
+                ...update.headers,
+                Authorization: `Bearer ${localStorage.jwt}`,
+              };
+            }
+            return update;
+          }
+   
+//   else {
+//     // Delete auth header
+//     delete axios.defaults.headers.common["Authorization"];
+//   }
+//};
 export default setAuthToken;
+
+// function updateOptions(options) {
+//     const update = { ...options };
+//     if (localStorage.jwt) {
+//       update.headers = {
+//         ...update.headers,
+//         Authorization: `Bearer ${localStorage.jwt}`,
+//       };
+//     }
+//     return update;
+//   }
+  
+//   export default function fetcher(url, options) {
+//     return fetch(url, updateOptions(options));
+//   }
