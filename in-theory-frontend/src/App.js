43f4b4eb08f815fcd88cc26f45457f9a8843b81f
Tsunamins <reillyamr@gmail.com>
2020-06-09 12:51:12 -0400

    add some stuff for later

--- in-theory-frontend/src/App.js
+++ in-theory-frontend/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux'
 import './App.css';
-
+import { Route, Switch } from "react-router-dom";
 import Landing from "./components/layout/Landing";
-import Register from "./components/auth/Register";
+import Register from "./components/auth/Signup";
 import Login from "./components/auth/Login";
+
+// import jwt_decode from "jwt-decode";
+// import setAuthToken from "./utils/setAuthToken";
+// import { setCurrentUser, logoutUser } from "./actions/authActions";
 
 class App extends React.Component {
 
