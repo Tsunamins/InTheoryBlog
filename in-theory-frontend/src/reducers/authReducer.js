692ef85376588142cc408a26ec7ef9e4bf39b0f3
Tsunamins <reillyamr@gmail.com>
2020-06-09 15:13:30 -0400

    fix error with action

--- in-theory-frontend/src/reducers/authReducer.js
+++ in-theory-frontend/src/reducers/authReducer.js
@@ -1,26 +1,33 @@
 
   const initialState = {
     isAuthenticated: false,
-    user: {},
-    loading: false
+    user_id: {},
+    loading: false,
+    user: {}
   };
   export default function(state = initialState, action) {
+    console.log(action)
     switch (action.type) {
+       
         case "SET_CURRENT_USER":
             return {
                 ...state,
                 isAuthenticated: true,
-                user: action.user
+                user_id: action.decoded
+                
             };
+            
         case "LOGOUT_USER":
             return initialState
         
-        case "USER_LOADING":
+        case "LOAD_CURRENT_USER":
             return {
                 ...state,
-                loading: true
+                loading: true,
+                user: action.user
             };
       default:
         return state;
     }
+    
   }
