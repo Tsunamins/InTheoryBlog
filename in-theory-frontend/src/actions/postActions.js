a2633c73b62d37ffd79c6ffad69689129d3f2639
Tsunamins <reillyamr@gmail.com>
2020-06-12 11:56:54 -0400

    add to actions get all posts connect to reducer

--- in-theory-frontend/src/actions/postActions.js
+++ in-theory-frontend/src/actions/postActions.js
@@ -8,7 +8,61 @@ export const createPost = (postData, history) => dispatch => {
         },
         body: JSON.stringify(postData)
     })
-    .then(resp => console.log(resp.json()))
+    .then(resp => resp.json())
+    
     // .then(resp => history.push("/:id"))
+    .then(response => {
+        if(response.error){
+          alert(response.error)
+        } else {
+          dispatch(addPost(response))
+        }
+      })
+      .catch(console.log)
   
 }
+
+export const getPosts = () => dispatch => {  
+    
+       return fetch('http://localhost:5000/api/v1/posts')
+        .then(resp => resp.json())
+        .then(response => {
+            if(response.error){
+                alert(response.error)
+            } else {
+       
+              dispatch(getAllPosts(response))
+            }
+            })
+            .catch(console.log)
+          
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+//action creators
+
+export const addPost = post => {    
+    return {
+        type: "ADD_POST",
+        post 
+    }
+  }
+
+  export const getAllPosts = posts => {    
+    return {
+        type: "GET_POSTS",
+        posts 
+    }
+  }
+
