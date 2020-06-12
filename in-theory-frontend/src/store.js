ccb110315fe79f2d99da0649cae7ecabb09f1208
Tsunamins <reillyamr@gmail.com>
2020-06-12 11:54:14 -0400

    add postReducer to store

--- in-theory-frontend/src/store.js
+++ in-theory-frontend/src/store.js
@@ -1,10 +1,12 @@
 import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import thunk from "redux-thunk";
 import authReducer from './reducers/authReducer'
+import postReducer from './reducers/postReducer'
 
 const reducer = combineReducers({
 
-    authReducer
+    authReducer,
+    postReducer
  })
 
 
