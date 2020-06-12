f15794feb77552f8937b3df860fa04659566516f
Tsunamins <reillyamr@gmail.com>
2020-06-12 10:30:57 -0400

    create general reducer for posts

---
+++ in-theory-frontend/src/reducers/postReducer.js
@@ -0,0 +1,17 @@
+export default function post(state = [], action) {
+   
+    switch (action.type) {
+      case 'GET_POSTS':  
+        return action.post
+      case 'ADD_POST':
+         
+        return state.concat(action.post)
+      case 'DELETE_POST':
+        return  state.filter(post => post.id === action.post ? false : true)
+      case 'UPDATE_POST':
+        return state.map(post => post.id === action.post.id ? action.post : post)
+        
+        default:
+            return state
+        }
+      }
