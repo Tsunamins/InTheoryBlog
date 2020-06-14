eb0c0b064e3e6e6a1fa93fdc2c46a6bef0a35c73
Tsunamins <reillyamr@gmail.com>
2020-06-14 15:47:26 -0400

    change layout/navigation to more spec comps

---
+++ in-theory-frontend/src/components/layout/AuthNav.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+
+const AuthNav = () => (
+ 
+        <div className="col" >
+            <li className="list-inline-item btn btn-outline-info">
+              <Link to="/signup">Sign Up</Link>
+            </li>
+        
+            <li className="list-inline-item btn btn-outline-info">
+              <Link to="/login">Log In</Link>
+            </li>
+           
+        </div>
+      
+    );
+
+export default AuthNav;
