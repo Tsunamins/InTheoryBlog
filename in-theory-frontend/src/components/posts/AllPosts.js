a4537c7c9e5146780763cb1211feb639a0889d4b
Tsunamins <reillyamr@gmail.com>
2020-06-14 15:48:38 -0400

    display posts based on use of component

--- in-theory-frontend/src/components/posts/AllPosts.js
+++ in-theory-frontend/src/components/posts/AllPosts.js
@@ -3,18 +3,35 @@ import { Link } from "react-router-dom";
 
 
 const AllPosts = (props) => {
+ 
+  console.log(props)
+  let posts = []
+  let heading = "Posts"
+  
+  if(Object.keys(props)[0] === 'posts'){
+      heading = "All Posts"
+      posts = props.posts
+  } else if(Object.keys(props)[0] === 'myposts'){
+      heading = "My Posts"
+      posts = props.myposts
+  } else {
+    heading = `${props.author}'s Posts`
+    posts = props.userposts
+  }
+  
+    
 
-     const posts = props.posts.map(p => 
+
+     const postsList = posts.map(p => 
     <div key={p._id}>
         <Link to={`/posts/${p._id}`}>{p.title}</Link></div>
     )
      
   return(
-      <div>
-          <Link to="/">Home</Link>
-        <hr></hr>
+      <div className="shadow-sm p-3 mb-5 bg-white rounded">
+        <h3>{heading}</h3>
         <div>
-            {posts}
+            {postsList}
         </div>
       </div>
       
